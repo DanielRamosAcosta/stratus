6d3b90671d17159ac3ba492f58fe1f2d272fd695
Daniel Ramos <danielramosacosta1@gmail.com>
2025-07-27 00:30:08 +0200

    test: update email address in example.spec.ts and change expected welcome message

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,10 +4,10 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   await page.getByRole('button', { name: 'Sign In with Dex' }).click();
-  await page.getByPlaceholder(/email address/i).fill('john.doe@example.com');
+  await page.getByPlaceholder(/email address/i).fill('dani@example.com');
   await page.getByPlaceholder(/password/i).fill('password');
   await page.getByRole('button', { name: /Login/i }).click();
   await page.getByRole('button', { name: /Grant Access/i }).click();
 
-  await expect(page.getByText("Welcome to Remix")).toBeVisible();
+  await expect(page.getByText("Home")).toBeVisible();
 });
