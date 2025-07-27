fff8dfaa573a6ebdc3dd9de1da8e6df014299799
Daniel Ramos <danielramosacosta1@gmail.com>
2025-07-27 18:34:19 +0200

    add fixture

---
+++ tests/oidc.ts
@@ -0,0 +1,26 @@
+const userInfoFixture = {
+  email: "danielramosacosta1@gmail.com",
+  email_verified: true,
+  family_name: "Ramos Acosta",
+  given_name: "Daniel",
+  groups: ["admins", "dev"],
+  locale: "es-ES",
+  name: "Dani",
+  picture:
+    "https://2.gravatar.com/avatar/bd9cf3cfa5c4875128bdd435d7f304403c6c883442670a1cd201abf85d3858d1?size=512&d=initials",
+  preferred_username: "dani",
+  rat: 1753633683,
+  sub: "24aa99e8-28f2-463f-bf2e-a972dd95a2f2",
+  updated_at: 1753633693,
+  zoneinfo: "Europe/Madrid",
+};
+
+const introspectionFixture = {
+  active: true,
+  client_id: "stratus",
+  exp: 1753637293,
+  iat: 1753633692,
+  scope: "openid email profile groups",
+  sub: "24aa99e8-28f2-463f-bf2e-a972dd95a2f2",
+  username: "dani",
+};
