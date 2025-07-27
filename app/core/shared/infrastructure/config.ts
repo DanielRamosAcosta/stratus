e95b4cd3064ebfe804d185343563fd0238fdc686
Daniel Ramos <danielramosacosta1@gmail.com>
2025-07-27 16:51:23 +0200

    enhance configuration schema with database settings

--- app/core/shared/infrastructure/config.ts
+++ app/core/shared/infrastructure/config.ts
@@ -6,6 +6,11 @@ const ConfigSchema = z.object({
   OAUTH_ISSUER_URL: z.string(),
   OAUTH_CLIENT_ID: z.string(),
   OAUTH_CLIENT_SECRET: z.string(),
+  DB_HOSTNAME: z.string(),
+  DB_PORT: z.string().default("5432").transform(Number),
+  DB_USERNAME: z.string().default("stratus"),
+  DB_PASSWORD: z.string(),
+  DB_DATABASE_NAME: z.string().default("stratus"),
 });
 
 export const config = ConfigSchema.parse(process.env);
